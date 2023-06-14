e85e1152fd2454388c96c5b909267e5618ddb8e3
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-06-13 19:43:48 -0700

    Setup BookList Component, Added Single Book

--- src/index.js
+++ src/index.js
@@ -1,20 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function Greeting() {
+const BookList = () => {
   return (
-    <div>
-      <Person />
-      <Message />
-    </div>
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
   );
-}
+};
 
-const Person = () => <h2>Jo Doe</h2>;
-const Message = () => {
-  return <p>Message, here!</p>;
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.hLWTRrkKv6cnPSmO7n-xowAAAA%26pid%3DApi&f=1&ipt=25250b75e7d0bb32e367899893754d9fdb477c4d5b42507cca2ae3975c7f28f3&ipo=images"
+    alt="The Haunting of Hill House"
+  />
+);
+const Title = () => {
+  return <h2>The Haunting of Hill House</h2>;
+};
+const Author = () => {
+  return <h4>Shirley Jackson</h4>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Greeting />);
+root.render(<BookList />);
