3db862a0dd63ea0c0fc152c0364a33260f3c6dd5
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-06-13 21:45:45 -0700

    Added book props

--- src/index.js
+++ src/index.js
@@ -1,38 +1,71 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./css/index.css";
+
+// const author = "Shirley Jackson";
+// const title = "We Have Always Lived in the Castle";
+// const img = require("./img/books/book3.jpg");
+
+const firstBook = {
+  author: "Shelby Van Pelt",
+  title: "Remarkably Bright Creatures",
+  img: require("./img/books/book1.jpg"),
+};
+
+const secondBook = {
+  author: "Grady Hendrix",
+  title: "How to Sell a Haunted House",
+  img: require("./img/books/book2.jpg"),
+};
+
+const thirdBook = {
+  author: "Shirley Jackson",
+  title: "We Have Always Lived in the Castle",
+  img: require("./img/books/book3.jpg"),
+};
+
+const fourthBook = {
+  author: "Min Jin Lee",
+  title: "Pachinko",
+  img: require("./img/books/book4.jpg"),
+};
 
 const BookList = () => {
   return (
-    <section>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+    <section className="booklist">
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      />
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
+      <Book
+        author={thirdBook.author}
+        title={thirdBook.title}
+        img={thirdBook.img}
+      />
+      <Book
+        author={fourthBook.author}
+        title={fourthBook.title}
+        img={fourthBook.img}
+      />
     </section>
   );
 };
 
-const Book = () => {
+const Book = (props) => {
+  console.log(props);
   return (
-    <article>
-      <Image />
-      <Title />
-      <Author />
+    <article className="book">
+      <img src={props.img} alt={props.title} />
+      <h2>{props.title}</h2>
+      <h4>{props.author}</h4>
     </article>
   );
-};
-
-const Image = () => (
-  <img
-    src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.hLWTRrkKv6cnPSmO7n-xowAAAA%26pid%3DApi&f=1&ipt=25250b75e7d0bb32e367899893754d9fdb477c4d5b42507cca2ae3975c7f28f3&ipo=images"
-    alt="The Haunting of Hill House"
-  />
-);
-const Title = () => {
-  return <h2>The Haunting of Hill House</h2>;
-};
-const Author = () => {
-  return <h4>Shirley Jackson</h4>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
