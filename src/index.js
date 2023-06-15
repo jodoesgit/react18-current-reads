1f0b254b549c7644896f36729c37d8677e063a89
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-06-14 22:05:19 -0700

    Exploration: Log single book title using props

--- src/index.js
+++ src/index.js
@@ -36,7 +36,6 @@ const books = [
 const BookList = () => {
 	return (
 		<section className="booklist">
-			<EventExamples />
 			{books.map((book) => {
 				return <Book {...book} key={book.id} />;
 			})}
@@ -44,31 +43,17 @@ const BookList = () => {
 	);
 };
 
-const EventExamples = () => {
-	return (
-		<section>
-			<form>
-				<h2>Form</h2>
-				<input
-					type="text"
-					name="product"
-					onChange={(e) => console.log(e.target.value)}
-					style={{ margin: "1rem 0" }}
-				/>
-				<button type="submit">Submit</button>
-				<div>
-					<button onClick={() => console.log("clicked!")}>Click me</button>
-				</div>
-			</form>
-		</section>
-	);
-};
+const Book = (props) => {
+	const { img, title, author } = props;
+	const displayTitle = () => {
+		console.log(title);
+	};
 
-const Book = ({ img, title, author }) => {
 	return (
 		<article className="book">
 			<img src={img} alt={title} />
 			<h2>{title}</h2>
+			<button onClick={displayTitle}>display title</button>
 			<h4>{author}</h4>
 		</article>
 	);
