9dfaf44b7c42b35bcdeb20a8f9107e0f5710f563
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-06-14 23:08:50 -0700

    Moved books to separate file then imported them in (books.js)

---
+++ src/books.js
@@ -0,0 +1,26 @@
+export const books = [
+	{
+		author: "Shelby Van Pelt",
+		title: "Remarkably Bright Creatures",
+		img: require("./img/books/book1.jpg"),
+		id: 1,
+	},
+	{
+		author: "Grady Hendrix",
+		title: "How to Sell a Haunted House",
+		img: require("./img/books/book2.jpg"),
+		id: 2,
+	},
+	{
+		author: "Shirley Jackson",
+		title: "We Have Always Lived in the Castle",
+		img: require("./img/books/book3.jpg"),
+		id: 3,
+	},
+	{
+		author: "Min Jin Lee",
+		title: "Pachinko",
+		img: require("./img/books/book4.jpg"),
+		id: 4,
+	},
+];
--- src/index.js
+++ src/index.js
@@ -1,62 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./css/index.css";
-
-// const author = "Shirley Jackson";
-// const title = "We Have Always Lived in the Castle";
-// const img = require("./img/books/book3.jpg");
-
-const books = [
-	{
-		author: "Shelby Van Pelt",
-		title: "Remarkably Bright Creatures",
-		img: require("./img/books/book1.jpg"),
-		id: 1,
-	},
-	{
-		author: "Grady Hendrix",
-		title: "How to Sell a Haunted House",
-		img: require("./img/books/book2.jpg"),
-		id: 2,
-	},
-	{
-		author: "Shirley Jackson",
-		title: "We Have Always Lived in the Castle",
-		img: require("./img/books/book3.jpg"),
-		id: 3,
-	},
-	{
-		author: "Min Jin Lee",
-		title: "Pachinko",
-		img: require("./img/books/book4.jpg"),
-		id: 4,
-	},
-];
+import { books } from "./books";
 
 const BookList = () => {
-	const getBook = (id) => {
-		const book = books.find((book) => book.id === id);
-		console.log(book);
-	};
 	return (
 		<section className="booklist">
 			{books.map((book) => {
-				return <Book {...book} key={book.id} getBook={getBook} />;
+				return <Book {...book} key={book.id} />;
 			})}
 		</section>
 	);
 };
 
 const Book = (props) => {
-	const { img, title, author, getBook, id } = props;
-	const getSingleBook = () => {
-		getBook(id);
-	};
+	const { img, title, author } = props;
 	return (
 		<article className="book">
 			<img src={img} alt={title} />
 			<h2>{title}</h2>
-			<button onClick={() => getBook(id)}>Add to Cart</button>
 			<h4>{author}</h4>
 		</article>
 	);
